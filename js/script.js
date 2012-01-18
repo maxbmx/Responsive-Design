0fce5db7ba53014785361dfe6fda93e8dc051e22
Maksymilian Sleziak <maksymilian.sleziak@gmail.com>
2012-01-18 21:16:57 +0100

    First commit and push

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+		$("#wrap").text($("#wrap").width()+"px");
+	
+	$(window).resize(function()
+	{
+		$("#wrap").text($("#wrap").width()+"px");
+	});	
+
+});
+
+
+
+
+
+
